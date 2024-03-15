e4518564d47bc1c159a31d24044e73974aa5d8c3
BlackBear69420 <107828414+BlackBear69420@users.noreply.github.com>
2024-03-15 22:41:12 +0530

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4'>
+        <nav>
+           <ul>
+            <li>
+                <Link href='/' aria-label='Home Page'>
+                    Sumit Nayak
+                </Link>
+            </li>
+            <li>
+              <Link href='/About'>
+                About
+              </Link>  
+            </li>
+            <li>
+              <Link href='/Projects'>
+                Projects
+              </Link>  
+            </li>
+            </ul> 
+        </nav>
+    </header>
+  )
+}
+
+export default Header
